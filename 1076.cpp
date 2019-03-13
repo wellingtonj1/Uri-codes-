d968cdc9300be0ec43021074b524d8efe0d79b27
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2019-03-13 11:51:33 -0300

    Update 1076.cpp

--- 1076.cpp
+++ 1076.cpp
@@ -4,6 +4,21 @@
 
 using namespace std;
 
+void jconect(int mat1[][2],int ligas)
+{
+    int p=0,q=0;
+
+    for(p=0;p<ligas;p++)
+    {
+        for(q=0;q<ligas;q++)
+        {
+            cout<<mat1[p][q]<<" ";
+        }
+            cout<<endl;
+    }
+
+}
+
 int main()
 {
     int primeiro,ligacoes,tamanho,vertis,x,y,i,j,aux,coloca;
@@ -35,20 +50,21 @@ int main()
         }
     }
 
-    for(i=0;i<tamanho;i++)
+   /* for(i=0;i<tamanho;i++)
     {
         for(j=0;j<tamanho;j++)
         {
             cout<<mat[i][j]<<" ";
         }
             cout<<endl;
-    }
+    }*/
 
     while(leutudo!=false)
     {
         x=primeiro;
         y=primeiro;
 
+        jconect(conectados,ligacoes);  //na lateral direita tem?
 
 
     }
