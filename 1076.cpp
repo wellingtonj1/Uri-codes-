2c74b5af8673fd2996a4193dd41b02549a1b983e
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2019-03-13 14:14:00 -0300

    Update 1076.cpp

--- 1076.cpp
+++ 1076.cpp
@@ -4,69 +4,86 @@
 
 using namespace std;
 
-void jconect(int mat1[][2],int ligas)
+bool buscaigual(int mat1[][2],int ligas,int at,int nex)
 {
     int p=0,q=0;
     for(p=0;p<ligas;p++)
     {
         for(q=0;q<2;q++)
         {
-            cout<<mat1[p][q]<<" ";
+            if((mat1[p][q]==at && mat1[p][q+1]==nex)||mat1[q][p]==at&&mat1[q+1][p]==nex)
+            {
+                return true;
+            }
         }
-            cout<<endl;
     }
+    return false;
 
 }
 
 int main()
 {
-    int primeiro,ligacoes,tamanho,vertis,x,y,i,j,aux,coloca;
-    cin>>primeiro;
-    cin>>vertis;
-    cin>>ligacoes;
+    int primeiro,ligacoes,tamanho,vertis,x,y,i,j,aux,coloca,grafos,atual,proxcompa,final,conts;
+    final=0;
+    cin>>grafos;
 
-    bool leutudo=false;
-
-    tamanho=sqrt(vertis);
-    int mat[tamanho][tamanho];
-    int conectados[ligacoes][2];
-
-    for(i=0;i<ligacoes;i++)
+    int contador=0;
+    conts=0;
+    while(final!=grafos)
     {
-        for(j=0;j<2;j++)
+        cin>>primeiro;
+        cin>>vertis;
+        cin>>ligacoes;
+        tamanho=sqrt(vertis);
+        int mat[tamanho][tamanho];
+        int conectados[ligacoes][2];
+
+        for(i=0;i<ligacoes;i++)
         {
-            cin>>conectados[i][j];
+            for(j=0;j<2;j++)
+            {
+                cin>>conectados[i][j];
+            }
         }
+
+        coloca=0;
+        for(i=0;i<tamanho;i++)
+        {
+            for(j=0;j<tamanho;j++)
+            {
+                mat[i][j]=coloca;
+                coloca++;
+            }
+        }
+
+       /*for(i=0;i<tamanho;i++)
+        {
+            for(j=0;j<tamanho;j++)
+            {
+                cout<<mat[i][j]<<" ";
+            }
+                cout<<endl;
+        }*/
+
+       for(x=0;x<tamanho;x++)
+       {
+           for(y=0;y<tamanho;y++)
+           {
+                atual=conectados[x][y];
+                proxcompa=conectados[x][y+1];
+                if(buscaigual(conectados,ligacoes,atual,proxcompa)) //na lateral direita tem?
+                {
+                    cout<<"esse é igual == "<<conectados[x][y]<<" "<<conectados[x][y+1]<<endl;
+                    contador++;
+                }
+           }
+        }
+
+       cout<<"\n\n"<<contador<<endl;
     }
 
-    coloca=0;
-    for(i=0;i<tamanho;i++)
-    {
-        for(j=0;j<tamanho;j++)
-        {
-            mat[i][j]=coloca;
-            coloca++;
-        }
-    }
 
-   /* for(i=0;i<tamanho;i++)
-    {
-        for(j=0;j<tamanho;j++)
-        {
-            cout<<mat[i][j]<<" ";
-        }
-            cout<<endl;
-    }*/
 
-    while(leutudo==false)
-    {
-        x=primeiro;
-        y=primeiro;
-
-        jconect(conectados,ligacoes);  //na lateral direita tem?
-        leutudo=true;
-
-    }
 
 
 }
