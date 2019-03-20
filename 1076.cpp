e83ad0a5f0127ecdd3dc15b48f6f72621a3c1a1f
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2019-03-20 10:32:01 -0300

    Update 1076.cpp

--- 1076.cpp
+++ 1076.cpp
@@ -4,97 +4,64 @@
 
 using namespace std;
 
-/*bool buscaigual(int mat1[][2],int ligas,int at,int nex)
+#define true 1;
+#define false 0;
+
+int buscaigual(int mat1[][2],int ligas)
 {
     int p=0,q=0;
-    for(p=0;p<ligas;p++)
+    int i,y;
+    i=y=0;
+    int iguais=0;
+
+    while(i<ligas)
     {
-        for(q=0;q<2;q++)
+        for(p=0;p<ligas;p++)
         {
-            if((mat1[p][q]==at && mat1[p][q+1]==nex)||mat1[q][p]==at&&mat1[q+1][p]==nex)
+            for(q=0;q<2;q++)
             {
-                return true;
+                if(mat1[p][q]==mat1[i][y])
+                {
+                    iguais++;
+                }
             }
         }
+        y++;
+        if(y>=2)
+        {
+            y=0;
+            i++;
+        }
     }
-    return false;
-
-}*/
+    return iguais;
+}
 
 int main()
 {
-    int primeiro,ligacoes,tamanho,vertis,x,y,i,j,aux,coloca,grafos,atual,proxcompa,final,conts,var1,var2;
-    final=0;
-    cin>>grafos;
+    int valorverti=0,ligacoes=0,quantivoltas=0,contador=0,temporario;
+    cin>>quantivoltas;
+    int armazen[quantivoltas];
 
-    int contador=0;
-    conts=0;
-    var1=var2=0;
-    tamanho=1;
-    while(var1<tamanho)
+    while(quantivoltas!=contador)
     {
-        cin>>primeiro;
-        cin>>vertis;
+        cin>>temporario;
+        cin>>temporario;
+
         cin>>ligacoes;
-        tamanho=sqrt(vertis);
-        int mat[tamanho][tamanho];
-        int conectados[ligacoes][2];
+        int matpadentro[ligacoes][2];
 
-        for(i=0;i<ligacoes;i++)
+        for(int i=0;i<ligacoes;i++)
         {
-            for(j=0;j<2;j++)
+            for(int y=0;y<2;y++)
             {
-                cin>>conectados[i][j];
+                cin>>matpadentro[i][y];
             }
         }
 
-        coloca=0;
-        for(i=0;i<tamanho;i++)
-        {
-            for(j=0;j<tamanho;j++)
-            {
-                mat[i][j]=coloca;
-                coloca++;
-            }
-        }
+        cout<<"\n\n"<<buscaigual(matpadentro,ligacoes)<<endl;
 
-       /*for(i=0;i<tamanho;i++)
-        {
-            for(j=0;j<tamanho;j++)
-            {
-                cout<<mat[i][j]<<" ";
-            }
-                cout<<endl;
-        }*/
-       if(var2<tamanho)
-       {
-           atual=conectados[var1][var2];
-           proxcompa=conectados[var1][var2+1];
-       }
-       else
-       {
-           var1++;
-           var2=0;
-           atual=conectados[var1][var2];
-           proxcompa=conectados[var1][var2+1];
-       }
-       for(x=0;x<tamanho;x++)
-       {
-           for(y=0;y<tamanho;y++)
-           {
-                if((conectados[x][y]==atual&&conectados[x][y+1]==proxcompa) || (conectados[y][x]==atual&&conectados[y][x+1]==proxcompa))
-                {
-                    cout<<"esse é igual == "<<conectados[x][y]<<" "<<conectados[x][y+1]<<endl;
-                    contador++;
-                }
-           }
-        }
-
-
+        contador++;
     }
-
-    cout<<"\n\n"<<contador*2<<endl;
-
 
 
 }
