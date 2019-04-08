66448db8a29dc7ac05b586b38cd1a5ff36862a73
JapaBits <34574051+wellingtonj1@users.noreply.github.com>
2019-04-08 11:54:56 -0300

    Create 1022.cpp

---
+++ 1022.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int quantiop,contador=0;
+    cin>>contador;
+
+
+
+    while(contador!=quantiop)
+    {
+        int numero,demoniador,opera;
+        cin>>numero;
+        cin>>opera;
+
+        
+        
+        contador++;
+    }
+
+}
